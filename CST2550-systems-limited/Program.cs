e8b182cf25d54dc41878bc7453d5920f208b691b
Daniel <danialkarimi509@gmail.com>
2025-02-02 19:36:24 +0000

    push program

--- CST2550-systems-limited/Program.cs
+++ CST2550-systems-limited/Program.cs
@@ -8,8 +8,103 @@ namespace CST2550_systems_limited
 {
     public class Program
     {
+        static bool tryAgain = true;
         static void Main(string[] args)
         {
+            UserManagement users = new UserManagement();
+
+            users.AddUser(1, "Jonathan", "j@j.com", 34);
+            users.AddUser(2, "Ammy", "a@a.com", 27);
+            users.AddUser(3, "Martin", "m@m.com", 18);
+            users.AddUser(4, "Joshua", "jo@jo.com", 24);
+            users.AddUser(5, "Adam", "ad@ad.com", 28);
+            users.AddUser(6, "Daniel", "d@d.com", 43);
+            users.AddUser(7, "Rinna", "r@r.com", 24);
+
+
+            List<User> usersList = users.GetUsers();
+            // Check if user exists
+            // If not throw an error, if user exists print their details 
+
+            try
+            {
+                Console.WriteLine("Search users and print the user with id 2:");
+                User UByID = users.FindUser(2);
+                if (UByID != null)
+                {
+                    Console.WriteLine(UByID.ToString());
+                    Console.WriteLine();
+                }
+                else
+                {
+                    throw new FormatException("User doesn't exist");
+                }
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+
+
+
+            Console.WriteLine("Before deletion:");
+            Console.WriteLine("User's name, email and age corresponding to the id:");
+            foreach (User user in usersList)
+            {
+                Console.WriteLine(user.ToString());
+            }
+
+
+            users.RemoveUser(3);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("After deletion:");
+            Console.WriteLine("User's name, email and age corresponding to the id:");
+            foreach (User user in users.GetUsers())
+            {
+                Console.WriteLine(user.ToString());
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Search users and print the user with age 24:");
+            List<User> UByAge = users.GetUsersByAge(24);
+            Console.WriteLine("Found {0} user/(s)", UByAge.Count);
+            foreach (User user in UByAge)
+            {
+                Console.WriteLine(user.ToString());
+            }
+
+            // A user attemps to update their email
+            while (tryAgain)
+            {
+                try
+                {
+                    tryAgain = false;
+                    Console.WriteLine();
+                    Console.WriteLine("Enter new email");
+                    string newEmail = Console.ReadLine();
+                    usersList[0].Email = newEmail;
+                    if (usersList[0].IsValidEmail(usersList[0].Email))
+                    {
+                        Console.WriteLine("User 1 details:");
+                        Console.WriteLine(usersList[0].ToString());
+                    }
+                    else
+                    {
+                        throw new FormatException("Incorrect email.");
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    tryAgain = true;
+                }
+
+            }
         }
     }
 }
